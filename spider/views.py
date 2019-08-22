5ca6fe8e759327b7fac016bbf84c3067d235776f
burningfish <273079347@qq.com>
2019-08-22 10:59:11 +0800

    *

--- spider/views.py
+++ spider/views.py
@@ -10,6 +10,6 @@ def index(request):
     if request.method == 'GET':
         return render(request, 'spider/index.html')
     if request.method == 'POST':
-        cmd = "python3 /home/burningfish/myProject/optimize_spider_v2.py >> /home/burningfish/myProject/spider_log/log.txt &"
+        cmd = "nohup python3 /home/burningfish/myProject/SpiderXuMing.py > /home/burningfish/myProject/spider_log/log.txt &"
         os.system(cmd)
         return HttpResponse("程序已经开始执行！请耐心等待邮件！")
