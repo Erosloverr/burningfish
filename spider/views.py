2187b6c45b29f5bd14c257cb11100d10c3c62a6b
burningfish <273079347@qq.com>
2019-08-22 15:12:07 +0800

    add only run onec!

--- spider/views.py
+++ spider/views.py
@@ -10,6 +10,12 @@ def index(request):
     if request.method == 'GET':
         return render(request, 'spider/index.html')
     if request.method == 'POST':
-        cmd = "nohup python3 /home/burningfish/myProject/SpiderXuMing.py > /home/burningfish/myProject/spider_log/log.txt &"
-        os.system(cmd)
-        return HttpResponse("程序已经开始执行！请耐心等待邮件！")
+        r = os.popen("ps -ef | grep SpiderXuMing.py | wc -l")
+        run_flag = r.read()
+        r.close()
+        if int(run_flag) >3:
+            return HttpResponse("程序已经开始执行！请耐心等待邮件！")
+        else:
+            cmd = "nohup python3 /home/burningfish/myProject/SpiderXuMing.py > /home/burningfish/myProject/spider_log/log.txt &"
+            os.system(cmd)
+            return HttpResponse("程序已经开始执行！请耐心等待邮件！")
