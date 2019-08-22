fcee889652f6c02215cd3579cfb2dda9ec7dd83e
burningfish <273079347@qq.com>
2019-08-22 15:17:32 +0800

    *

--- spider/views.py
+++ spider/views.py
@@ -14,7 +14,7 @@ def index(request):
         run_flag = r.read()
         r.close()
         if int(run_flag) >3:
-            return HttpResponse("程序已经开始执行！请耐心等待邮件！")
+            return HttpResponse("已经有一个爬虫程序再执行了！请耐心等待邮件！")
         else:
             cmd = "nohup python3 /home/burningfish/myProject/SpiderXuMing.py > /home/burningfish/myProject/spider_log/log.txt &"
             os.system(cmd)
